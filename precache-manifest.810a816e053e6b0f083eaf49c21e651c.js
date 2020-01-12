7189bc85e4952cf1f787a90bbb5fc1c2de7d18d5
Coleman Stoltze <coleman.stoltze@gmail.com>
2020-01-12 16:15:08 -0500

    Updates

---
+++ precache-manifest.810a816e053e6b0f083eaf49c21e651c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d577e34d6d88b124f3948ed49fc69334",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12ae7629bf8066443f85",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "46ec609814b9a26f4abc",
+    "url": "/static/js/2.56e42a6a.chunk.js"
+  },
+  {
+    "revision": "6aeb469c0c20fd3334c86de961c4692c",
+    "url": "/static/js/2.56e42a6a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "12ae7629bf8066443f85",
+    "url": "/static/js/main.e611a659.chunk.js"
+  },
+  {
+    "revision": "b5dc7de2f06714fb9402",
+    "url": "/static/js/runtime-main.5c536247.js"
+  }
+]);

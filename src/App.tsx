9ca59a0f9753930443c4c71baa62931e27c522ee
Coleman Stoltze <coleman.stoltze@gmail.com>
2020-01-12 21:13:02 -0500

    prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import './App.css';
-import Timers from './components/Timers';
-import {lunchTimes} from './config';
+import React from "react";
+import "./App.css";
+import Timers from "./components/Timers";
+import { lunchTimes } from "./config";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <h1>North Ridge Lunch Schedule</h1>
-      <Timers lunchTimes={lunchTimes}/>
+      <Timers lunchTimes={lunchTimes} />
     </div>
   );
 };

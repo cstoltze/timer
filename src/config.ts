69a2769a42f93fd25a3e1ef01fae2071253dc6c3
Coleman Stoltze <coleman.stoltze@gmail.com>
2020-01-14 19:15:59 -0500

    Change time that grades go silent
    Add more silent time at end of lunch

--- src/config.ts
+++ src/config.ts
@@ -4,8 +4,8 @@ export const lunchTimes = [
     lunchTime: { start: "10:55 am", end: "11:30 am" },
     intervals: [
       { start: "10:55 am", end: "11:05 am", canTalk: false, showTimer: true },
-      { start: "11:05 am", end: "11:25 am", canTalk: true, showTimer: true },
-      { start: "11:25 am", end: "11:30 am", canTalk: false, showTimer: false }
+      { start: "11:05 am", end: "11:20 am", canTalk: true, showTimer: true },
+      { start: "11:20 am", end: "11:30 am", canTalk: false, showTimer: false }
     ]
   },
   {
@@ -13,8 +13,8 @@ export const lunchTimes = [
     lunchTime: { start: "11:35 am", end: "12:05 pm" },
     intervals: [
       { start: "11:35 am", end: "11:45 am", canTalk: false, showTimer: true },
-      { start: "11:45 am", end: "12:00 pm", canTalk: true, showTimer: true },
-      { start: "12:00 pm", end: "12:05 pm", canTalk: false, showTimer: false }
+      { start: "11:45 am", end: "11:58 am", canTalk: true, showTimer: true },
+      { start: "11:58 am", end: "12:05 pm", canTalk: false, showTimer: false }
     ]
   },
   {
@@ -22,8 +22,8 @@ export const lunchTimes = [
     lunchTime: { start: "12:35 pm", end: "01:05 pm" },
     intervals: [
       { start: "12:35 pm", end: "12:45 pm", canTalk: false, showTimer: true },
-      { start: "12:45 pm", end: "01:00 pm", canTalk: true, showTimer: true },
-      { start: "01:00 pm", end: "01:05 pm", canTalk: false, showTimer: false }
+      { start: "12:45 pm", end: "12:58 pm", canTalk: true, showTimer: true },
+      { start: "12:58 pm", end: "01:05 pm", canTalk: false, showTimer: false }
     ]
   },
   {
@@ -31,8 +31,8 @@ export const lunchTimes = [
     lunchTime: { start: "11:25 am", end: "11:55 am" },
     intervals: [
       { start: "11:25 am", end: "11:35 am", canTalk: false, showTimer: true },
-      { start: "11:35 am", end: "11:50 am", canTalk: true, showTimer: true },
-      { start: "11:50 am", end: "11:55 am", canTalk: false, showTimer: false }
+      { start: "11:35 am", end: "11:48 am", canTalk: true, showTimer: true },
+      { start: "11:48 am", end: "11:55 am", canTalk: false, showTimer: false }
     ]
   },
   {
@@ -40,8 +40,8 @@ export const lunchTimes = [
     lunchTime: { start: "12:20 pm", end: "12:50 pm" },
     intervals: [
       { start: "12:20 pm", end: "12:30 pm", canTalk: false, showTimer: true },
-      { start: "12:30 pm", end: "12:45 pm", canTalk: true, showTimer: true },
-      { start: "12:45 pm", end: "12:50 pm", canTalk: false, showTimer: false }
+      { start: "12:30 pm", end: "12:43 pm", canTalk: true, showTimer: true },
+      { start: "12:43 pm", end: "12:50 pm", canTalk: false, showTimer: false }
     ]
   },
   {
@@ -49,8 +49,8 @@ export const lunchTimes = [
     lunchTime: { start: "11:55 am", end: "12:30 pm" },
     intervals: [
       { start: "11:55 am", end: "12:05 pm", canTalk: false, showTimer: true },
-      { start: "12:05 pm", end: "12:25 pm", canTalk: true, showTimer: true },
-      { start: "12:25 pm", end: "12:30 pm", canTalk: false, showTimer: false }
+      { start: "12:05 pm", end: "12:20 pm", canTalk: true, showTimer: true },
+      { start: "12:20 pm", end: "12:30 pm", canTalk: false, showTimer: false }
     ]
   }
 ];
